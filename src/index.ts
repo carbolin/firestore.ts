b8deb086a865b5a7947cb58578cac2c660805008
lars <lmaischein@gmail.com>
2020-08-17 23:03:56 +0200

    updated collection uploader

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import { AppInitiator } from './app-initiator';
 import { firebaseConfig } from './firebase-config';
 import { DbInitiator } from './db-initiator';
 import { CollectionUploader } from './upload/collection-uploader';
-import { deutschland } from '../deutschland';
 import { liechtenstein } from '../liechtenstein';
 
 const firestore = new DbInitiator(firebaseConfig);
