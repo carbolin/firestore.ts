96dd44b02aed0b80dc748141a9ef8a4f8e0718b6
lars <lmaischein@gmail.com>
2020-08-17 22:49:22 +0200

    updated collection uploader

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,12 @@ import { firebaseConfig } from './firebase-config';
 import { DbInitiator } from './db-initiator';
 import { CollectionUploader } from './upload/collection-uploader';
 import { deutschland } from '../deutschland';
+import { liechtenstein } from '../liechtenstein';
 
 const firestore = new DbInitiator(firebaseConfig);
 const db = firestore.dbInit();
 
-const uploader = new CollectionUploader(db, 'halal');
+const uploader = new CollectionUploader(db, 'lkf');
 
-uploader.upload(deutschland)
-    .then(() => console.log('nice'))
-
-    .catch((e) => console.log(e.message, 'Error'));
+uploader.upload(liechtenstein);
 
--- src/upload/collection-uploader.ts
+++ src/upload/collection-uploader.ts
@@ -1,25 +1,32 @@
-import { Uploader } from '../models/Uploader';
+import { Uploader } from './Uploader';
 import * as firebase from 'firebase/app';
 
 export class CollectionUploader implements Uploader {
 
     constructor(private db: firebase.firestore.Firestore, private collection: string) { }
 
-    async upload(data: any[]) {
+    async upload(data: any[]): Promise<void> {
 
         const batch: firebase.firestore.WriteBatch = this.db.batch();
 
-        const collectionRef: firebase.firestore.CollectionReference<firebase.firestore.DocumentData> = this.db.collection(this.collection);
+        const collectionRef: firebase.firestore.CollectionReference<firebase.firestore.DocumentData>
+            = this.db.collection(this.collection);
 
-        data.forEach(async (doc: firebase.firestore.DocumentData) => {
-            const docRef: firebase.firestore.DocumentReference<firebase.firestore.DocumentData> = collectionRef.doc();
+        data.forEach((doc: firebase.firestore.DocumentData) => {
+            const docRef: firebase.firestore.DocumentReference<firebase.firestore.DocumentData>
+                = collectionRef.doc();
+
             batch.set(docRef, doc);
         });
 
-        await batch.commit();
-
-        console.log(`Uploaded ${data.length} Documents finished`);
-
+        try {
+            await batch.commit();
+            console.log(`Uploaded ${data.length} Documents finished`);
+            process.exit(0);
+        }
+        catch (e) {
+            console.log(e.message, 'Error');
+            process.exit(-1);
+        }
     }
-
 }
