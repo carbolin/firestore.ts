8622e2271bc6df81810ff68e81c198bad38141f4
lars <lmaischein@gmail.com>
2020-08-19 20:43:07 +0200

    unpdated index

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,13 @@ import { AppInitiator } from './app-initiator';
 import { firebaseConfig } from './firebase-config';
 import { DbInitiator } from './db-initiator';
 import { FirestoreUploader } from './upload/firestore-uploader';
-import { liechtenstein } from '../liechtenstein';
+import { liechtenstein } from '../data/liechtenstein';
+import { countries } from '../data/conutries';
 
 const firestore = new DbInitiator(firebaseConfig);
 const db = firestore.dbInit();
 
 const uploader = new FirestoreUploader(db, 'test');
 
-uploader.upload(liechtenstein);
+uploader.upload(countries);
 
